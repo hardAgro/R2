486b5bef63c0da16a5b35d1911869f68ef6115d1
Carlos Henrique Lima <45734747+Carlos-Digibits@users.noreply.github.com>
2018-12-09 16:19:53 -0300

    Arquivos do Arduino

---
+++ R2_Inovafarm2018-ESP.ino
@@ -0,0 +1,114 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+
+//-------------------VARIABLES GLOBALES--------------------------
+int contconexion = 0;
+
+int cont = 0;
+String str = "";
+String a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p = "";
+
+int tamanho, I = 0;
+
+const char *ssid = "R2 Sistemas";
+const char *password = "R2SISTEMAS";
+
+unsigned long previousMillis = 0;
+
+char host[48];
+String strhost = "10.10.10.120";
+String strurl = "dae/insert_weather.php";
+String chipid = "A";
+
+//-------FUNÇÃO PARA ENVIAR DADOS PARA O MYSQL--------
+
+String enviardados(String datos) {
+
+
+  String linea = "error";
+  WiFiClient client;
+  strhost.toCharArray(host, 49);
+  if (!client.connect(host, 80)) {
+    Serial.println("Fallo de conexion");
+    return linea;
+  }
+
+  client.print(String("POST ") + strurl + " HTTP/1.1" + "\r\n" + 
+               "Host: " + strhost + "\r\n" +
+               "Accept: */*" + "*\r\n" +
+               "Content-Length: " + datos.length() + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+               "\r\n" + datos);           
+  delay(1);             
+  
+  Serial.print("Enviando dados a MySQL...");
+  Serial.print(datos);
+
+
+
+
+}
+
+//-------------------------------------------------------------------------
+
+void setup() {
+
+  // Inicia Serial
+  Serial.begin(9600);
+  Serial.println("");
+
+  Serial.print("chipId: "); 
+  Serial.println(chipid); 
+
+  // Conexión WIFI
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { //Cuenta hasta 50 si no se puede conectar lo cancela
+    ++contconexion;
+    delay(500);
+    Serial.print(".");
+  }
+  if (contconexion <50) {
+      //para usar con ip fija
+      IPAddress ip(10,10,10,156); 
+      IPAddress gateway(10,10,10,1); 
+      IPAddress subnet(255,255,255,0); 
+      WiFi.config(ip, gateway, subnet); 
+      
+      Serial.println("");
+      Serial.println("WiFi conectado");
+      Serial.println(WiFi.localIP());
+  }
+  else { 
+      Serial.println("");
+      Serial.println("Error de conexão");
+  }
+}
+
+//----------------------------LOOP--------------------------------
+void loop() {
+  cont++;
+
+
+
+    String newStr = Serial.readStringUntil('\n');
+
+      if (newStr != "") {
+        str = newStr;
+        if (cont == 100){
+          enviardados("status="+str);
+          Serial.println(str);
+          cont=0;
+            }
+          }
+        
+  
+        
+      
+    
+
+    delay(10);
+    str="";
+
+
+
+}
---
+++ R2_Inovafarm2018-UNO.ino
@@ -0,0 +1,107 @@
+
+#include <SoftwareSerial.h> // Biblioteca SoftwareSerial
+#include "DHT.h"
+
+#define DHTPIN A0 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+//------------------------difinir pinos de dos SENSORES-------------------
+//int sen_temp = A0;
+int sen_umi = A1;
+int sen_condut = A2;
+float t = 0;
+
+const int BUFFER_SIZE = 1000;
+
+
+//----------------------VARIÁVEIS QUE RECEBEM OS VALORES MEDIDOS PELOS SENSORES---------------------------------
+float temperatura, entrada, umidade, condut, cond, cond1 = 0;
+int umed = 0;
+
+//----------------------IDENTIFICAÇÃO DA ÁREA (A = ÁREA 1, B = ÁREA 2 E ASSIM SUCESSIVAMENTE)----------
+String ID = "1";
+
+//--------------------------------------------------------------------------------------------------------------
+//defines gerais
+#define TEMPO_LEITURA_SENSOR  1  //tempo entre leituras
+
+
+SoftwareSerial comunicacaoESP(2, 3); // objeto para comunicação serial com o ESP8266 nas Portas 2(RX) e 3(TX)
+
+int cont = 0;
+String env,  A, B, C, D = "";
+
+
+//Função setup (inicializações do programa)
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("ARDUINO + ENVIO DADOS POR SERIAL >> ESP");
+  comunicacaoESP.begin(9600); // inicia a comunicação serial com o ESP8266
+  
+}
+
+//loop (programa principal)
+void loop()
+{
+
+  cont++; //VARIÁVEL QUE GERENCIA A LEITURA
+  
+  t = dht.readTemperature();
+  umed = analogRead(sen_umi);
+  umidade = map(umed,0,1023,-100,-1);
+  umidade = umidade/100;
+  
+  delay(1);
+  
+  cond = analogRead(sen_condut);
+  cond1 = map(cond,190,300,250,-100);
+  condut = cond1/100;
+  if (cont == 50){ //QUANDO IGUAL A 5(RESULTANDO EM 100ms), DISPARA A LEITURA
+////////////////////////////////////////////////  
+    
+    temperatura = readTemperatureWithBuffer();
+
+    
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+    Serial.print("*C - ");
+    Serial.print("Pressão solo: ");
+    Serial.print(umidade);
+    Serial.print("bar - ");
+    Serial.print("Nível sais: ");
+    Serial.print(condut);
+    Serial.print(" mho/cm - ");
+    Serial.println(cond);
+  }
+  delay(1);
+
+//////////////////////////////////    
+  
+//----------APÓS 120ms ENVIA A RESPOSTA DOS SENSORES---------------
+if (cont == 55){
+
+      A=ID+","+String(temperatura)+","+String(umidade)+","+String(condut);
+      comunicacaoESP.println(A);  //ENVIA AO ESP PELA PORTA SERIAL SECUNDÁRIA A STRING
+      comunicacaoESP.flush(); 
+      Serial.println(A);          //PRINTA NA SERIAL PARA MONITORAMENTO
+      cont=0;                      //ZERA A VARIÁVEL DE DISPARO
+      
+}
+  delay(TEMPO_LEITURA_SENSOR);      //DELAY(1ms)
+
+}
+
+float readTemperatureWithBuffer(){
+
+  float buffer = 0;
+  for (int i = 0; i < BUFFER_SIZE; i++){
+      buffer += t;
+  }
+  return (buffer/BUFFER_SIZE);
+}
+
+
+
